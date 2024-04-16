b15da305f509a8d8c0a395452fa2b5dc96727e8d
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-04-16 21:40:28 +0530

    fix: 🐛 remove mobile

--- apps/mobile/src/features/nfts/item/traits.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { NFTTraitCard } from 'src/features/nfts/item/traits'
-import { render } from 'src/test/test-utils'
-import { NFTTrait } from 'wallet/src/test/gqlFixtures'
-
-it('renders trait card', () => {
-  const tree = render(<NFTTraitCard trait={NFTTrait} />)
-  expect(tree).toMatchSnapshot()
-})
