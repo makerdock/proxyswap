5a0cca42c6632eec447ddbfc1f7a474733dc6394
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-04-23 00:27:36 +0530

    fix: added 0.25% fees

--- src/components/FeeSelector/shared.tsx
+++ src/components/FeeSelector/shared.tsx
@@ -8,25 +8,18 @@ export const FEE_AMOUNT_DETAIL: Record<
   { label: string; description: ReactNode; supportedChains: SupportedChainId[] }
 > = {
   [FeeAmount.LOWEST]: {
-    label: '0.01',
-    description: <Trans>Best for very stable pairs.</Trans>,
-    supportedChains: [
-      // SupportedChainId.MAINNET,
-      // SupportedChainId.POLYGON,
-      // SupportedChainId.POLYGON_MUMBAI,
-      // SupportedChainId.CELO,
-      // SupportedChainId.CELO_ALFAJORES,
-      // SupportedChainId.OPTIMISM,
-    ],
-  },
-  [FeeAmount.LOW]: {
     label: '0.05',
     description: <Trans>Best for stable pairs.</Trans>,
     supportedChains: ALL_SUPPORTED_CHAIN_IDS,
   },
-  [FeeAmount.MEDIUM]: {
+  [FeeAmount.LOW]: {
     label: '0.3',
     description: <Trans>Best for most pairs.</Trans>,
+    supportedChains: ALL_SUPPORTED_CHAIN_IDS,
+  },
+  [FeeAmount.MEDIUM]: {
+    label: '0.25',
+    description: <Trans>Best for very stable pairs.</Trans>,
     supportedChains: ALL_SUPPORTED_CHAIN_IDS,
   },
   [FeeAmount.HIGH]: {
